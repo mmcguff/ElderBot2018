687210c950301193d29c381bd58ffe22c25fdb33
Matthew McGuff <matthewmcguff85@gmail.com>
2018-04-07 00:39:48 -0600

    real code for chat bot iframe

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
+echo '<!DOCTYPE html>';
 echo '<html>';
 echo '<head>';
-echo '<title>PHP Test</title>';
+echo '   <meta charset="utf-8" />';
+echo '    <meta http-equiv="X-UA-Compatible" content="IE=edge">';
+echo '    <title>Elder Bot</title>';
+echo '    <meta name="viewport" content="width=device-width, initial-scale=1">';
+echo '    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />';
+echo '   <script src="main.js"></script>';
 echo '</head>';
 echo '<body>';
-  echo '<p>Hello World</p>';  
+echo '    <!--Insert this in the iframe source to get this working-->';
+echo '    <!-- https://webchat.botframework.com/embed/elderbot?s=ijaB88sOVCY.cwA.cWY.gO6q1l3Y28a_nqaUirA0DZwzs18SM2pd5co4UfDRsCc -->';
+    
+echo '<iframe width="800" height="800" src=\'https://webchat.botframework.com/embed/elderbot?s=ijaB88sOVCY.cwA.cWY.gO6q1l3Y28a_nqaUirA0DZwzs18SM2pd5co4UfDRsCc\'></iframe>';
+
+
+
+
 echo '</body>';
 echo '</html>';
-
 ?>
