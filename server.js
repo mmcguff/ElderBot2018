8c35d932611dbcdb1a40a8ccea5498ea568149b9
Matthew McGuff <matthewmcguff85@gmail.com>
2018-04-07 00:30:28 -0600

    trying to use php to deploy the app.

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-var port = 8080;
-
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-
-app.listen(process.env.PORT || port, () => console.log('Example app listening on port 3000!'))
