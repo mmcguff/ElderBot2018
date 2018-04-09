bc66208b7c8b9f4529b69ff388f9d8b0dc5c7979
Matthew McGuff <matthewmcguff85@gmail.com>
2018-04-09 08:16:49 -0600

    add some styling

--- index.php
+++ index.php
@@ -1,23 +1,49 @@
 <?php
 echo '<!DOCTYPE html>';
 echo '<html>';
+
 echo '<head>';
-echo '   <meta charset="utf-8" />';
-echo '    <meta http-equiv="X-UA-Compatible" content="IE=edge">';
+echo '    <meta charset="utf-8" />';
+echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
 echo '    <title>Elder Bot</title>';
 echo '    <meta name="viewport" content="width=device-width, initial-scale=1">';
-echo '    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />';
-echo '   <script src="main.js"></script>';
+echo '    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+echo '    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">';
+echo '    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">';
+echo '<link rel="icon" href="favicon.ico" type="image/x-icon">';
 echo '</head>';
+echo '<style>';
+echo '    body{';
+echo '        background-color: #f37d25;';    
+echo '    }';
+
+echo '    h1, p{';
+echo '        text-align: center;';
+echo '        color: white;';        
+echo '    }';
+
+echo '    iframe{';
+echo '        background-color: honeydew;';
+echo '    }';
+
+
+echo '</style>';
+
 echo '<body>';
-echo '    <!--Insert this in the iframe source to get this working-->';
-echo '    <!-- https://webchat.botframework.com/embed/elderbot?s=ijaB88sOVCY.cwA.cWY.gO6q1l3Y28a_nqaUirA0DZwzs18SM2pd5co4UfDRsCc -->';
-    
-echo '<iframe width="800" height="800" src=\'https://webchat.botframework.com/embed/elderbot?s=ijaB88sOVCY.cwA.cWY.gO6q1l3Y28a_nqaUirA0DZwzs18SM2pd5co4UfDRsCc\'></iframe>';
 
+echo '    <div class="w3-container w3-center">';
+echo '        <h1>Chat with Elder Bot</h1>';
+echo '        <p>Type in the chat below to get started.</p>';
 
+echo '        <!--Insert this in the iframe source to get this working-->';
+echo '        <!--  -->';
+
+echo '        <iframe height="600" width="350" class="embed-responsive-item" src="https://webchat.botframework.com/embed/elderbot?s=ijaB88sOVCY.cwA.cWY.gO6q1l3Y28a_nqaUirA0DZwzs18SM2pd5co4UfDRsCc"></iframe>';
+
+echo '    </div>';
 
 
 echo '</body>';
+
 echo '</html>';
 ?>
